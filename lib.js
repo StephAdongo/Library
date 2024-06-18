430c550986b93caaa2252715ac4130191fae268a
StephAdongo <stephanie@kabarak.ac.ke>
2024-06-18 13:41:24 +0300

    add .

--- lib.js
+++ lib.js
@@ -1,52 +1,27 @@
 const myLibrary = [];
+const cardContainer = document.querySelector(".card-container");
+const showBtn = document.querySelector(".add-book");
+const modal = document.querySelector(".modal");
+const addBtn = document.getElementById("new-book");
 
- function Book (title,author,pages,read) {
+Book.id = 0;
+
+function Book (title, author, pages, read) {
+    this.bookId = `book${++Book.id}`;
     this.title = title;
-    this.author = author; 
+    this.author = author;
     this.pages = pages;
     this.read = read;
-
 }
-function addBookToLibrary (myLibrary) {
-   myLibrary.push (new Book ("The Lord of the Rings", "J.R.R. Tolkien", "696"));
-   myLibrary.push (new Book ("Pride and Prejudice", "Jane Austen", "920"));
-   myLibrary.push (new Book ("To Kill a Morckingbird", "Haper Lee", "1030"));
-   console.log ("Books added to the library !");
+
+function addBookToLibrary(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  
+  const cardTitle = document.createElement("h2");
+  cardTitle.textContent = `Title: ${book.title}`;
 
   
-  displayBooks(myLibrary);
-  document.getElementById("addBookForm").style.display = "none";
-
-   }
-
-function displayBooks (myLibrary) {
-    const booksContainer = document.getElementById("books");
-    booksContainer.innerHTML = "";
-
-    for (const book of myLibrary) {
-        const bookCard = document.createElement ("div");
-        bookCard.classList.add("book-card");
-        let bookInfo =`<h3>${book.title}</h3><p>By ${book.author}</p>`;
-        bookInfo +=`<p>Pages : ${book.pages}</p>`;
-        if (book.read) {
-            bookInfo +=`<p>Read</p>`;
-        }
-        else {
-            bookInfo += `<Not Read</p>`;
-        }
-        bookCard.innerHTML = bookInfo;
-        booksContainer.appendChild(bookCard);
-    }
 }
 
-addBookToLibrary(myLibrary);
-displayBooks(myLibrary);
-
-
-
-     
-
-
-
-
-
+ 
